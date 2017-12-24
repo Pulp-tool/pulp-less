71a04c3e4eb76ca070210a770372a624631a50b4
Mark Kimsal <metrofindings@gmail.com>
2017-12-24 11:54:42 -0500

    source

---
+++ src/Pulp/Less.php
@@ -0,0 +1,25 @@
+<?php
+namespace Pulp;
+
+use Pulp\DataPipe;
+
+class Less  extends DataPipe { 
+
+	public $parser;
+
+	public function __construct($opts) {
+		$this->parser = new \Less_Parser();
+	}
+
+    public function __invoke($data) { 
+		echo "got invoked...\n";
+    } 
+
+	public function write($data) {
+		$this->parser->parseFile( $data->getPathName() );
+	}
+
+    public function end($data=null) { 
+		$this->emit('data', [$this->parser->getCss()]);
+    } 
+}
